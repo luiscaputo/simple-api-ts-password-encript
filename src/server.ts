094a60015faf398ca8f9130e9ed1cba537d90848
luiscaputo <luiscaputo15@gmail.com>
2021-06-06 02:08:46 +0100

    first-commit **subindo**

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+const express = require('express')
+// import express from 'express'
+// import './database'
+const app = express()
+
+  app.get('/', (req, res) => {
+    return res.json({"message": "retorna"});
+  })
+
+app.listen(3333, () =>{
+    const sms = 'testando o ts-node-dev'
+    console.log(sms)
+})
