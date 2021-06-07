2d2d033b9fb739f155c41697e5698180ab1fa62a
luiscaputo <luiscaputo15@gmail.com>
2021-06-07 04:35:47 +0100

    in-progress-route-generate-qrcode

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,37 @@
-const express = require('express')
-// import express from 'express'
-// import './database'
+import { json } from "express";
+const express = require('express');
+import { QRCode } from 'qrcode'
+const fs = require('fs');
+
 const app = express()
+app.use(express(json))
+  // app.get('/:text', async (req, res) => {
+  //   const text = req.params
+  //   const salt = await bcrypt.genSalt(10)
+  //   const cript = await bcrypt.hash(text, salt)
 
-  app.get('/', (req, res) => {
-    return res.json({"message": "retorna"});
-  })
-
+  //   return res.json({cript});
+  // })
+  app.get("/qrcode", (require, response) => {
+    const dataPerson = 'simpleText'
+    //const qrcode = QRCode.toDataURL("http://asyncawait.net");
+    const res = QRCode.toString(
+      JSON.stringify(dataPerson),
+      { type: "svg" },
+      function (err, url) {
+        try{
+          console.log(url);
+          response.send(url);
+        }catch(err){
+          console.log(err);
+          response.send(err);
+        }
+      }
+    );
+  });
+  
 app.listen(3333, () =>{
     const sms = 'testando o ts-node-dev'
     console.log(sms)
 })
+//res.status(200).send({sucess: true, data: url})
